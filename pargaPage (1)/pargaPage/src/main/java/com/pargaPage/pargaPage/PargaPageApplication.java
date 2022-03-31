7a8adb16e919a4029b096f211d08762716e55661
Eli-ssaD <notjusthandbags@hotmail.co.uk>
2022-03-31 16:13:46 +0100

    parga page

---
+++ pargaPage (1)/pargaPage/src/main/java/com/pargaPage/pargaPage/PargaPageApplication.java
@@ -0,0 +1,13 @@
+package com.pargaPage.pargaPage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PargaPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PargaPageApplication.class, args);
+	}
+
+}
---
+++ pargaPage (1)/pargaPage/src/test/java/com/pargaPage/pargaPage/PargaPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pargaPage.pargaPage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PargaPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
